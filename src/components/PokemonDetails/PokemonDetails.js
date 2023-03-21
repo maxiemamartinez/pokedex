61af5e77bae3f421a8acbc30b814be9c3f872cb6
martinezmaxiema@gmail.com <maximiliano@Maximilianos-MacBook-Pro.local>
2023-03-21 15:59:09 -0300

    WIP Pokemon details

--- src/components/PokemonDetails/PokemonDetails.js
+++ src/components/PokemonDetails/PokemonDetails.js
@@ -1,14 +1,21 @@
 import React from "react";
 import colors from "../../utils/types";
 import capitalizeName from "../../utils/capitalizeName";
+import { useNavigate } from "react-router-dom";
 
 import "./pokemon-details.css";
 
 function PokemonDetails({ id, name, picture, types }) {
-  
+  const navigate = useNavigate();
+
+  const handleNavigate = () => navigate(`/pokemons/${id}`);
+
   return (
-    <div>
-      <div className="picture" style={{ background: colors[types[0].type.name], opacity: 0.8 }}>
+    <div onClick={handleNavigate}>
+      <div
+        className="picture"
+        style={{ background: colors[types[0].type.name], opacity: 0.8 }}
+      >
         <img className="avatar" src={picture} />
       </div>
       <div className="description">
@@ -16,7 +23,11 @@ function PokemonDetails({ id, name, picture, types }) {
         <div className="names">{capitalizeName(name)}</div>
         <div className="pokemon-type">
           {types.map((x) => (
-            <div className="pokemon-style" key={x.type.name} style={{ backgroundColor: colors[x.type.name] }}>
+            <div
+              className="pokemon-style"
+              key={x.type.name}
+              style={{ backgroundColor: colors[x.type.name] }}
+            >
               {capitalizeName(x.type.name)}
             </div>
           ))}
--- src/pages/PokemonDetails/PokemonDetails.js
+++ src/pages/PokemonDetails/PokemonDetails.js
@@ -2,16 +2,19 @@ import React, { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import axios from "axios";
 import "./pokemonIndividual.css";
+import capitalizeName from "../../utils/capitalizeName";
+import colors from "../../utils/types";
+
 export async function loader({ params }) {
   return params.pokemonId;
 }
 
 //const TABS_COMPONENTS = {
-  //forms: <Form />,
-  //details: <Details />,
-  //type: <Types />,
-  //stats: <Stats />,
-  //wear: <Wear/>
+//forms: <Form />,
+//details: <Details />,
+//type: <Types />,
+//stats: <Stats />,
+//wear: <Wear/>
 
 //};
 
@@ -20,7 +23,7 @@ const PokemonDetails = () => {
 
   const pokemonId = useLoaderData();
   const url = `http://pokeapi.co/api/v2/pokemon/${pokemonId}`;
-  const [pokemon, setPokemon] = useState([]);
+  const [pokemon, setPokemon] = useState(null);
 
   React.useEffect(() => {
     axios.get(url).then((response) => {
@@ -29,27 +32,51 @@ const PokemonDetails = () => {
   }, []);
   console.log(pokemon);
 
+  if (!pokemon) {
+    return <div>Loading</div>;
+  }
+
   return (
-    <div className="a">
+    <div className="root" >
       <div className="exhaust">
-      <div className="group">
-        <div>N  6</div>
-        <div>Charizard</div>
-      <div className="types">types</div>
+        <div className="group">
+          <div className="groupId">N{pokemon.id}</div>
+          <div className="groupName">{capitalizeName(pokemon.name)}</div>
+        </div>
+        <div>
+        <div className="types">
+        
+
+         {pokemon.types.map((x) => (
+            <div
+              key={x.type.name}
+              style={{ backgroundColor: colors[x.type.name] }}
+            >
+              {capitalizeName(x.type.name)}
+            </div>
+          ))}
       </div>
       </div>
-      <div className="sec"> 
-      <div className="pictureInd">Picture</div>
-      <div className="info">
-        <div onClick={() => setTab("forms")}>Forms</div>{" "}
-        <div onClick={() => setTab("details")}>Details</div> {" "}
-        <div onClick={() => setTab("types")}>Types</div>{" "}
-        <div onClick={() => setTab("Stats")}>Stats</div>{" "}
-        <div onClick={() => setTab("Wear")}>Wear</div>{" "}
-      <div className="descrip">description</div>
       </div>
+      <div className="sec">
+        <div className="pictureInd">
+          <img src={pokemon.sprites.other.dream_world.front_default}/>
+        </div>
+        <div className="third">
+          <div className="info">
+            <div onClick={() => setTab("forms")}>
+              Evolutions
+              <div className="evolutions">A</div>
+            </div>{" "}
+            <div onClick={() => setTab("details")}>Details</div>{" "}
+            <div onClick={() => setTab("types")}>Types</div>{" "}
+            <div onClick={() => setTab("Stats")}>Stats</div>{" "}
+            <div onClick={() => setTab("Wear")}>Wear</div>{" "}
+          </div>
+          <div className="descrip">First evolution</div>
+        </div>
       </div>
-      </div>
+    </div>
   );
 };
 export default PokemonDetails;
