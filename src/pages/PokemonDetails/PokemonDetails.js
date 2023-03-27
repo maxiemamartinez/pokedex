848ae8a027d5c480a4348fb7a8ac8def8900a554
martinezmaxiema@gmail.com <maximiliano@Maximilianos-MacBook-Pro.local>
2023-03-27 18:12:14 -0300

    Changes in evolution and add another values

--- src/pages/PokemonDetails/PokemonDetails.js
+++ src/pages/PokemonDetails/PokemonDetails.js
@@ -25,52 +25,66 @@ const PokemonDetails = () => {
   const pokemonId = useLoaderData();
   const url = `http://pokeapi.co/api/v2/pokemon/${pokemonId}`;
   const [pokemon, setPokemon] = useState(null);
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
 
   React.useEffect(() => {
     axios.get(url).then((response) => {
       setPokemon(response.data);
     });
   }, [url]);
- // console.log(pokemon);
+  // console.log(pokemon);
 
   if (!pokemon) {
     return <div>Loading</div>;
   }
 
   return (
-    <div className="root" >
+    <div className="root">
       <div className="exhaust">
         <div className="group">
           <div className="groupId">N{pokemon.id}</div>
           <div className="groupName">{capitalizeName(pokemon.name)}</div>
         </div>
         <div>
-        <div className="types">
-         {pokemon.types.map((x) => (
-            <div className="type"
-              key={x.type.name}
-              style={{ backgroundColor: colors[x.type.name] }}
-            >
-              {capitalizeName(x.type.name)}
-            </div>
-          ))}
-      </div>
-      </div>
+          <div className="types">
+            {pokemon.types.map((x) => (
+              <div
+                className="type"
+                key={x.type.name}
+                style={{ backgroundColor: colors[x.type.name] }}
+              >
+                {capitalizeName(x.type.name)}
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
       <div className="sec">
-        <div className="pictureInd" style={{ background: colors[pokemon.types[0].type.name], opacity: 0.8 }}>
-          <img src={pokemon.sprites.other.dream_world.front_default}/>
+        <div
+          className="pictureInd"
+          style={{
+            background: colors[pokemon.types[0].type.name],
+            opacity: 0.8,
+          }}
+        >
+          <img
+            src={
+              selectedPokemon || pokemon.sprites.other.dream_world.front_default
+            }
+          />
         </div>
         <div className="third">
           <div className="info">
-            <div className="bar" onClick={() => setTab("forms")}>Evolutions</div>{" "}
+            <div className="bar" onClick={() => setTab("forms")}>
+              Evolutions
+            </div>{" "}
             <div onClick={() => setTab("details")}>Details</div>{" "}
             <div onClick={() => setTab("types")}>Types</div>{" "}
             <div onClick={() => setTab("Stats")}>Stats</div>{" "}
             <div onClick={() => setTab("Wear")}>Wear</div>{" "}
           </div>
           <div className="results">
-          <PokemonEvolution/>
+            <PokemonEvolution onEvolutionClick={setSelectedPokemon} />
           </div>
           <div className="descrip">First evolution</div>
         </div>
--- src/pages/PokemonDetails/PokemonEvolution.js
+++ src/pages/PokemonDetails/PokemonEvolution.js
@@ -2,38 +2,72 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import colors from "../../utils/types";
 import { useLoaderData } from "react-router-dom";
+import COLORS from "../../utils/types";
 
-function getEvolutions(evolutions) {
+function getEvolutions(evolutions, currentName) {
   let evolutionsArray = [];
   let chain = evolutions.chain;
 
+  const getId = (url) => url.split("/").at(-2);
+
   while (chain) {
-    evolutionsArray.push(chain.species.name);
+    if (chain.species.name !== currentName) {
+      evolutionsArray.push({
+        id: getId(chain.species.url),
+        name: chain.species.name,
+      });
+    }
+
     chain = chain.evolves_to[0];
   }
 
   return evolutionsArray;
 }
 
-const PokemonEvolution = () => {
+const buildPicture = (id) =>
+  `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`;
+
+const PokemonEvolution = ({ onEvolutionClick }) => {
   const pokemonId = useLoaderData();
-  const url = `https://pokeapi.co/api/v2/evolution-chain/${pokemonId}`;
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
   const [evolutions, setEvolutions] = useState(null);
+  const [pokemon, setPokemon] = useState(null);
 
   React.useEffect(() => {
     axios.get(url).then((response) => {
-      const parsedEvolutions = getEvolutions(response.data);
-      setEvolutions(parsedEvolutions);
+      setPokemon(response.data);
+    });
+  }, [url]);
+
+  React.useEffect(() => {
+    axios.get(url).then((response) => {
+      const speciesUrl = response.data.species.url;
+      axios.get(speciesUrl).then((speciesResponse) => {
+        const evolutionChainUrl = speciesResponse.data.evolution_chain.url;
+        axios.get(evolutionChainUrl).then((evolutionResponse) => {
+          const parsedEvolutions = getEvolutions(
+            evolutionResponse.data,
+            response.data.name
+          );
+          setEvolutions(parsedEvolutions);
+        });
+      });
     });
   }, [url]);
 
   if (!evolutions) {
     return <div>Loading</div>;
   }
-
-  console.log("HERE CHECKING", evolutions);
-
-  return evolutions;
-
+  return evolutions.map((evolution) => (
+    <div key={evolution.name}>
+      {" "}
+      <img
+        className="evolutionPic"
+        onClick={() => onEvolutionClick(buildPicture(evolution.id))}
+        style={{ background: COLORS[pokemon.types[0].type.name], opacity: 0.8 }}
+        src={buildPicture(evolution.id)}
+      />
+    </div>
+  ));
 };
 export default PokemonEvolution;
